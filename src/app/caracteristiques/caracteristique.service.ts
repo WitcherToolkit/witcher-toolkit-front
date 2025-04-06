99c64f5cf8cde3c29f2e91fb9bc142921093663c
Mélody Delage <angie-island@hotmail.fr>
2025-04-06 23:45:57 +0200

    externalisation fonction part2

--- src/app/caracteristiques/caracteristique.service.ts
+++ src/app/caracteristiques/caracteristique.service.ts
@@ -2,6 +2,7 @@ import { Injectable, signal } from '@angular/core';
 import { Caracteristique } from '../models/caracteristique';
 import { CARACTERISTIQUE_LIST } from '../fake-data-set/caracteristiques-fake';
 import { Observable, of } from 'rxjs';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,14 @@ export class CaracteristiqueService {
   // Signal local (temporaire) pour l'état en mémoire
   private readonly caracteristiques = signal<Caracteristique[]>(CARACTERISTIQUE_LIST);
 
+  constructor(private fb: FormBuilder) {}
 
   getCaracteristiquesList(): Observable<Caracteristique[]> {
       return of(this.caracteristiques()); // Simule une requête HTTP
       //return this.http.get<Caracteristique[]>('url/api/caracteristiques');
   }
 
+  // Cette méthode permet de créer un groupe de formulaire pour une caractéristique
   searchCaracteristiques(term: string): Caracteristique[] {
     const lowerTerm = term.trim().toLowerCase();
     let filteredCaracteristiques: Caracteristique[] = [];
@@ -31,5 +34,71 @@ export class CaracteristiqueService {
 
     return filteredCaracteristiques;
   }
+
+  // Récupération des valeurs dérivées (PS, END, RÉC, ÉTOU) en fonction de la moyenne
+  getDerivedValues(average: number) {
+    const table: { [key: number]: { PS: number; END: number; RÉC: number; ÉTOU: number } } = {
+      2: { PS: 10, END: 10, RÉC: 2, ÉTOU: 2 },
+      3: { PS: 15, END: 15, RÉC: 3, ÉTOU: 3 },
+      4: { PS: 20, END: 20, RÉC: 4, ÉTOU: 4 },
+      5: { PS: 25, END: 25, RÉC: 5, ÉTOU: 5 },
+      6: { PS: 30, END: 30, RÉC: 6, ÉTOU: 6 },
+      7: { PS: 35, END: 35, RÉC: 7, ÉTOU: 7 },
+      8: { PS: 40, END: 40, RÉC: 8, ÉTOU: 8 },
+      9: { PS: 45, END: 45, RÉC: 9, ÉTOU: 9 },
+      10: { PS: 50, END: 50, RÉC: 10, ÉTOU: 10 },
+      11: { PS: 55, END: 55, RÉC: 10, ÉTOU: 10 },
+      12: { PS: 60, END: 60, RÉC: 10, ÉTOU: 10 },
+      13: { PS: 65, END: 65, RÉC: 10, ÉTOU: 10 },
+    };
+
+    return table[average] || { PS: 0, END: 0, RÉC: 0, ÉTOU: 0 };
+  }
+
+  // Récupération des valeurs de poings et pieds en fonction de la valeur de COR
+  getPoingsPiedsValues(corValue: number) {
+    if (corValue >= 1 && corValue <= 2) {
+      return { poings: '1D6 - 4', pieds: '1D6' };
+    } else if (corValue >= 3 && corValue <= 4) {
+      return { poings: '1D6 - 2', pieds: '1D6 + 2' };
+    } else if (corValue >= 5 && corValue <= 6) {
+      return { poings: '1D6', pieds: '1D6 + 4' };
+    } else if (corValue >= 7 && corValue <= 8) {
+      return { poings: '1D6 + 2', pieds: '1D6 + 6' };
+    } else if (corValue >= 9 && corValue <= 10) {
+      return { poings: '1D6 + 4', pieds: '1D6 + 8' };
+    } else if (corValue >= 11 && corValue <= 12) {
+      return { poings: '1D6 + 6', pieds: '1D6 + 10' };
+    } else if (corValue === 13) {
+      return { poings: '1D6 + 8', pieds: '1D6 + 12' };
+    } else {
+      return { poings: '', pieds: '' };
+    }
+  }
+
+   // Cette méthode met à jour la valeur actuelle des caractéristiques
+   /*updateValeurActuelle(caracteristiquePersonnage: FormArray): void {
+    caracteristiquePersonnage.controls.forEach(control => {
+      control.get('valeurActuelle')?.setValue(control.get('valeurMax')?.value);
+    });
+  }*/
+
+  // Externaliser la méthode createCaracteristiqueControl
+  /*createCaracteristiqueControl(code: string, isEditable: boolean): FormGroup {
+    const control = this.fb.group({
+      valeurMax: [isEditable ? 3 : { value: 0, disabled: true }, [Validators.required, Validators.min(3)]],
+      valeurActuelle: [isEditable ? 3 : 0],
+      code: [code]
+    });
+
+    if (isEditable) {
+      control.get('valeurMax')!.valueChanges.subscribe(() => {
+        // Logique mise à jour de la valeur actuelle
+        this.updateValeurActuelle(control);
+      });
+    }
+
+    return control;
+  }*/
 
 }
--- src/app/fiche-personnage/create-person/part2/part2.component.ts
+++ src/app/fiche-personnage/create-person/part2/part2.component.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { RACE_MAP } from '../../../fake-data-set/race-fake';
-import { CARACTERISTIQUE_LIST } from '../../../fake-data-set/caracteristiques-fake';
 import { PROFESSION_MAP } from '../../../fake-data-set/profession-fake';
 import { CaracteristiquePersonnage } from '../../../models/caracteristique-personnage';
+import { Caracteristique } from '../../../models/caracteristique';
+import { CaracteristiqueService } from '../../../caracteristiques/caracteristique.service';
 
 @Component({
   selector: 'app-part2',
@@ -16,14 +17,14 @@ import { CaracteristiquePersonnage } from '../../../models/caracteristique-perso
 })
 export class Part2Component implements OnInit, OnDestroy {
   @Input() form!: FormGroup;
-  caracteristiques = CARACTERISTIQUE_LIST;
+  caracteristiques : Caracteristique[] = [];
 
   subscriptions: Subscription[] = [];
   
   niveauJeu = signal<string>('libre');
   pointsRestants = signal<number>(0);
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private caracteristiqueService: CaracteristiqueService) {
     // Écoute les changements de niveau et met à jour les points restants
     effect(() => {
       this.updatePointsRestants();
@@ -32,12 +33,19 @@ export class Part2Component implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log(`Initialisation étape 2:`, this.form.value);
+
+    // Charger les caractéristiques depuis le service
+    this.caracteristiqueService.getCaracteristiquesList().subscribe((caracteristiques: Caracteristique[]) => {
+      this.caracteristiques = caracteristiques;
+    });
+
     this.initializeFormControls();
     this.calculateDerivedValues();
     this.subscribeToNiveauJeuChanges();
   }
 
   ngOnDestroy() {
+    // Annuler tous les abonnements pour éviter les fuites de mémoire
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
@@ -114,7 +122,7 @@ export class Part2Component implements OnInit, OnDestroy {
     const vitValue = this.caracteristiquePersonnage.at(vitIndex).get('valeurMax')?.value;
   
     const average = Math.floor((corValue + volValue) / 2);
-    const derivedValues = this.getDerivedValues(average);
+    const derivedValues = this.caracteristiqueService.getDerivedValues(average);
   
     this.setDerivedValue('PS', derivedValues.PS);
     this.setDerivedValue('END', derivedValues.END);
@@ -135,54 +143,13 @@ export class Part2Component implements OnInit, OnDestroy {
     this.setDerivedValue('COU', couValue);
     this.setDerivedValue('SAUT', sautValue);
   
-    const { poings, pieds } = this.getPoingsPiedsValues(corValue);
+    const { poings, pieds } = this.caracteristiqueService.getPoingsPiedsValues(corValue);
     this.form.get('poings')?.setValue(poings);
     this.form.get('pieds')?.setValue(pieds);
   
     // Mettre à jour la vigueur
     const vigueur = this.getVigueur();
     this.form.get('vigueur')?.setValue(vigueur);
-  }
-
-  // Récupération des valeurs dérivées (PS, END, RÉC, ÉTOU) en fonction de la moyenne
-  getDerivedValues(average: number) {
-    const table: { [key: number]: { PS: number; END: number; RÉC: number; ÉTOU: number } } = {
-      2: { PS: 10, END: 10, RÉC: 2, ÉTOU: 2 },
-      3: { PS: 15, END: 15, RÉC: 3, ÉTOU: 3 },
-      4: { PS: 20, END: 20, RÉC: 4, ÉTOU: 4 },
-      5: { PS: 25, END: 25, RÉC: 5, ÉTOU: 5 },
-      6: { PS: 30, END: 30, RÉC: 6, ÉTOU: 6 },
-      7: { PS: 35, END: 35, RÉC: 7, ÉTOU: 7 },
-      8: { PS: 40, END: 40, RÉC: 8, ÉTOU: 8 },
-      9: { PS: 45, END: 45, RÉC: 9, ÉTOU: 9 },
-      10: { PS: 50, END: 50, RÉC: 10, ÉTOU: 10 },
-      11: { PS: 55, END: 55, RÉC: 10, ÉTOU: 10 },
-      12: { PS: 60, END: 60, RÉC: 10, ÉTOU: 10 },
-      13: { PS: 65, END: 65, RÉC: 10, ÉTOU: 10 },
-    };
-
-    return table[average] || { PS: 0, END: 0, RÉC: 0, ÉTOU: 0 };
-  }
-
-  // Récupération des valeurs de poings et pieds en fonction de la valeur de COR
-  getPoingsPiedsValues(corValue: number) {
-    if (corValue >= 1 && corValue <= 2) {
-      return { poings: '1D6 - 4', pieds: '1D6' };
-    } else if (corValue >= 3 && corValue <= 4) {
-      return { poings: '1D6 - 2', pieds: '1D6 + 2' };
-    } else if (corValue >= 5 && corValue <= 6) {
-      return { poings: '1D6', pieds: '1D6 + 4' };
-    } else if (corValue >= 7 && corValue <= 8) {
-      return { poings: '1D6 + 2', pieds: '1D6 + 6' };
-    } else if (corValue >= 9 && corValue <= 10) {
-      return { poings: '1D6 + 4', pieds: '1D6 + 8' };
-    } else if (corValue >= 11 && corValue <= 12) {
-      return { poings: '1D6 + 6', pieds: '1D6 + 10' };
-    } else if (corValue === 13) {
-      return { poings: '1D6 + 8', pieds: '1D6 + 12' };
-    } else {
-      return { poings: '', pieds: '' };
-    }
   }
 
   getVigueur(): number {
