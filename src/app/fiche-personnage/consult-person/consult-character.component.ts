d97ad39c31b9560b3c139ffa99e34563e36e01ce
Mélody Delage <angie-island@hotmail.fr>
2025-06-16 19:15:07 +0200

    affichage fiche créée

--- src/app/fiche-personnage/consult-person/consult-character.component.ts
+++ src/app/fiche-personnage/consult-person/consult-character.component.ts
@@ -1,18 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component, Injectable } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { CaracteristiqueService } from '../../caracteristiques/caracteristique.service';
 
 @Component({
   selector: 'app-consult-character',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './consult-character.component.html',
-  styles: []
+  styleUrls: ['./consult-character.component.scss']
 })
-export class ConsultCharacterComponent {
+export class ConsultCharacterComponent implements AfterViewInit {
 
   characterData: any;
 
-  constructor() {
+  constructor(private caracteristiqueService: CaracteristiqueService) {
     this.characterData = history.state.data;
+  }
+
+  isEditableCaracteristiques() {
+  return this.characterData.caracteristiquePersonnage.filter((carac: any) =>
+    this.caracteristiqueService.isEditable(carac.code)
+  );
+  }
+
+  nonEditableCaracteristiques() {
+    return this.characterData.caracteristiquePersonnage.filter((carac: any) =>
+      !this.caracteristiqueService.isEditable(carac.code)
+    );
+  }
+
+  ngAfterViewInit() {
+    const tabs = document.querySelectorAll('.tabs');
+    M.Tabs.init(tabs);
   }
 }
