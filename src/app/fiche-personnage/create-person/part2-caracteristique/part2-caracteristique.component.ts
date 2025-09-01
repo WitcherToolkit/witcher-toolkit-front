b1c8f74e910c7c6a8f56a3555fbd57b4e19c2b86
Mélody Delage <angie-island@hotmail.fr>
2025-09-02 01:49:39 +0200

    correctif part2

--- src/app/fiche-personnage/create-person/part2-caracteristique/part2-caracteristique.component.ts
+++ src/app/fiche-personnage/create-person/part2-caracteristique/part2-caracteristique.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, OnDestroy, Input, signal, effect } from '@angular/core';
+import { Component, effect, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Caracteristique } from '../../../models/caracteristique';
+import { CaracteristiquePersonnage } from '../../../models/caracteristique-personnage';
 import { CaracteristiqueService } from '../../../caracteristiques/caracteristique.service';
 import { ToolsService } from '../../../tools/tools.service';
 
@@ -28,6 +29,10 @@ export class Part2CaracteristiqueComponent implements OnInit, OnDestroy {
       this.updatePointsRestants();
     });
   }
+  ngOnDestroy(): void {
+    // Désabonne tous les abonnements pour éviter les fuites de mémoire
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 
   ngOnInit() {
     console.log(`Initialisation étape 2:`, this.form.value);
@@ -35,24 +40,31 @@ export class Part2CaracteristiqueComponent implements OnInit, OnDestroy {
     // Charger les caractéristiques depuis le service
     this.caracteristiqueService.getCaracteristiquesList().subscribe((caracteristiques: Caracteristique[]) => {
       this.caracteristiques = caracteristiques;
+      this.initializeFormControls();
+      this.caracteristiqueService.calculateDerivedValues(this.caracteristiquePersonnage, this.caracteristiques, this.form);
+      this.subscribeToNiveauJeuChanges();
     });
-
-    this.initializeFormControls();
-    this.caracteristiqueService.calculateDerivedValues(this.caracteristiquePersonnage, this.caracteristiques, this.form);
-    this.subscribeToNiveauJeuChanges();
   }
 
-  ngOnDestroy() {
-    // Annuler tous les abonnements pour éviter les fuites de mémoire
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+  // Récupération du FormArray des caractéristiques
+  get caracteristiquePersonnage(): FormArray {
+    return this.form.get('caracteristiquePersonnage') as FormArray;
+  }
+
+  /** Retourne la liste des contrôles du FormArray correspondant aux caractéristiques principales */
+  get getCaracteristiquesPrincipalesList() {
+    return this.caracteristiquePersonnage.controls.filter((ctrl, i) => {
+      const code = ctrl.get('code')?.value;
+      const carac = this.caracteristiques.find(c => c.code === code);
+      return carac && carac.type === 'Principale';
+    });
   }
 
   // Initialisation des contrôles du formulaire
   private initializeFormControls() {
     const caracteristiquePersonnageArray = this.fb.array(
-      this.caracteristiques.map(caracteristique => this.createCaracteristiqueControl(caracteristique.code))
+      this.caracteristiques.map(caracteristique => this.createCaracteristiqueControl(caracteristique))
     );
-
     this.form.addControl('caracteristiquePersonnage', caracteristiquePersonnageArray);
     this.form.addControl('poings', this.fb.control(''));
     this.form.addControl('pieds', this.fb.control(''));
@@ -60,33 +72,24 @@ export class Part2CaracteristiqueComponent implements OnInit, OnDestroy {
     this.form.addControl('vigueur', this.fb.control(''));
   }
 
-  // Création d'un contrôle pour une caractéristique
-  private createCaracteristiqueControl(code: string): FormGroup {
-    const isEditable = this.caracteristiqueService.isEditable(code);
-    const initialValue = isEditable ? 3 : 0;
-    const control = this.fb.group({
-      valeurMax: [initialValue, [Validators.required, Validators.min(3)]],
-      valeurActuelle: [initialValue],
-      code: [code]
+   private createCaracteristiqueControl(carac: Caracteristique, valeur?: number): FormGroup {
+    const initialValue = valeur ?? 3;
+    const group = this.fb.group({
+      code: this.fb.control(carac.code),
+      valeurMax: this.fb.control(initialValue, [Validators.required, Validators.min(3), Validators.max(10)]),
+      valeurActuelle: this.fb.control(initialValue)
     });
-
-    if (isEditable) {
-      this.subscriptions.push(
-        control.get('valeurMax')!.valueChanges.subscribe(() => {
-          this.caracteristiqueService.updateValeurActuelle(this.caracteristiquePersonnage);
-          this.caracteristiqueService.calculateDerivedValues(this.caracteristiquePersonnage, this.caracteristiques, this.form);
-          this.updatePointsRestants();
-        })
-      );
-    } else {
-      control.get('valeurMax')!.valueChanges.subscribe(val => {
-        control.get('valeurActuelle')?.setValue(val, { emitEvent: false });
-      });
+    // Synchronise toujours valeurMax et valeurActuelle
+    group.get('valeurMax')!.valueChanges.subscribe(val => {
+      group.get('valeurActuelle')?.setValue(val, { emitEvent: false });
+    });
+    // Pour les secondaires, désactive les champs (readonly)
+    if (carac.type !== 'Principale') {
+      group.get('valeurMax')!.disable();
+      group.get('valeurActuelle')!.disable();
     }
-
-    return control;
+    return group;
   }
-
   // Abonnement aux changements du niveau de jeu
   private subscribeToNiveauJeuChanges() {
     this.subscriptions.push(
@@ -97,11 +100,14 @@ export class Part2CaracteristiqueComponent implements OnInit, OnDestroy {
     );
   }
 
-  // Récupération du FormArray des caractéristiques
-  get caracteristiquePersonnage(): FormArray {
-    return this.form.get('caracteristiquePersonnage') as FormArray;
+  /** Retourne la liste des contrôles du FormArray correspondant aux caractéristiques secondaires */
+  get getCaracteristiquesSecondairesList() {
+    return this.caracteristiquePersonnage.controls.filter((ctrl, i) => {
+      const code = ctrl.get('code')?.value;
+      const carac = this.caracteristiques.find(c => c.code === code);
+      return carac && carac.type === 'Secondaire';
+    });
   }
-
   /** Retourne la liste des contrôles du FormArray caracteristiquePersonnage */
   get getCaracteristiquesList() {
     return this.caracteristiquePersonnage.controls;
@@ -168,7 +174,36 @@ export class Part2CaracteristiqueComponent implements OnInit, OnDestroy {
     this.updatePointsRestants(); // Met à jour les points restants
   }
 
-  isEditable(code: string): boolean {
-    return this.caracteristiqueService.isEditable(code);
+  /** Initialise le FormArray caracteristiquePersonnage à partir d'une liste de caractéristiques */
+  initCaracteristiques(caracs: Caracteristique[]) {
+    this.caracteristiques = caracs;
+    // Supprime l'ancien FormArray s'il existe
+    if (this.form.contains('caracteristiquePersonnage')) {
+      this.form.removeControl('caracteristiquePersonnage');
+    }
+    const caracteristiquePersonnageArray = this.fb.array(
+      this.caracteristiques.map(caracteristique => this.createCaracteristiqueControl(caracteristique))
+    );
+    this.form.addControl('caracteristiquePersonnage', caracteristiquePersonnageArray);
+  }
+
+  /** Initialise le FormArray caracteristiquePersonnage à partir d'une liste de caractéristiquesPersonnage (pour édition) */
+  initCaracteristiquesPersonnage(caracsPerso: CaracteristiquePersonnage[]) {
+    this.caracteristiques = caracsPerso.map(cp => cp.caracteristique);
+    if (this.form.contains('caracteristiquePersonnage')) {
+      this.form.removeControl('caracteristiquePersonnage');
+    }
+    const caracteristiquePersonnageArray = this.fb.array(
+      caracsPerso.map(cp => this.createCaracteristiqueControl(cp.caracteristique, cp.valeurMax))
+    );
+    this.form.addControl('caracteristiquePersonnage', caracteristiquePersonnageArray);
+  }
+
+  getCaracteristiquePersonnageArray(): CaracteristiquePersonnage[] {
+    return this.caracteristiquePersonnage.controls.map((ctrl, i) => ({
+      valeurActuelle: ctrl.get('valeurActuelle')?.value,
+      valeurMax: ctrl.get('valeurMax')?.value,
+      caracteristique: this.caracteristiques[i]
+    }));
   }
 }
