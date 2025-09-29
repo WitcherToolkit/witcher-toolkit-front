501c00b86592ecfd91e77a484e766f78e4f180a0
Mélody Delage <angie-island@hotmail.fr>
2025-09-29 18:51:24 +0200

    correction authentification

--- src/app/shared/shared-services/jwt.interceptor.ts
+++ src/app/shared/shared-services/jwt.interceptor.ts
@@ -1,13 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const JwtInterceptor: HttpInterceptorFn = (req, next) => {
-
   const token = localStorage.getItem('jwt');
-
   if (token) {
+    const authHeader = `Bearer ${token}`;
+    console.log('[JWT INTERCEPTOR] Token:', token);
+    console.log('[JWT INTERCEPTOR] Authorization header:', authHeader);
     req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
+      setHeaders: { Authorization: authHeader }
     });
+  } else {
+    console.log('[JWT INTERCEPTOR] Aucun token trouvé');
   }
   return next(req);
 };
